8e3075e582b130f186c40334edad80deafa273bf
alejandroJGH <3056574880301@ingnieria.usac.edu.gt>
2020-08-21 23:58:39 -0600

    Tarea 2

---
+++ Tarea_2/LectorJson.py
@@ -0,0 +1,12 @@
+import json
+
+def CargarDatos(ruta):
+    with open(ruta) as contenido:
+        DatosJson = json.load(contenido)
+        for atributo in DatosJson:
+            print(atributo.get('nombre'))
+            print(atributo.get('Carnet'))
+            print(atributo.get('Carrera'))
+            print(atributo.get('Escuela'))
+            print('')
+
